c6172c31c058b0a92bede033a10974b4861b8585
Franco Arratia <franck_ral@hotmail.com>
2020-06-30 18:42:40 -0400

    share-knowledge-schedule-service: Integrated shedule-service with configuration-service, fixed issues.

--- demo-v1/sn-schedule-service/Startup.cs
+++ demo-v1/sn-schedule-service/Startup.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Steeltoe.Discovery.Client;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
 
-namespace sn_schedule_service
+
+using Steeltoe.Discovery.Client;
+namespace ShareKnowledge.ScheduleService
 {
     public class Startup
     {
@@ -27,9 +28,10 @@ namespace sn_schedule_service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDiscoveryClient(Configuration);
+
             services.AddControllers();
         }
-
+ 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -38,17 +40,15 @@ namespace sn_schedule_service
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
 
+
+       
+            app.UseDiscoveryClient();
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-            app.UseDiscoveryClient();
         }
     }
 }

178a0b59542621f92587b5d3866ffd9d1cacb216
Franco Arratia <franck_ral@hotmail.com>
2020-07-01 12:48:56 -0400

    share-knowledge-course-service: Integrated Eureka Client to Course Service implemented in nodejs with TypeScript

--- demo-v1/sn-course-service/resources/course.resource.ts
+++ demo-v1/sn-course-service/resources/course.resource.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { SERVER_PORT } from '../constants/environment';
+import { Eureka } from "eureka-js-client";
 
 export default class CourseResource {
     public app: express.Application;
@@ -8,6 +9,37 @@ export default class CourseResource {
     constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        let eurekaClient = new Eureka({
+            instance: {
+                app: 'course-service',
+                instanceId: 'course-service',
+                hostName: 'localhost',
+                ipAddr: '127.0.0.1',
+                statusPageUrl: `http://localhost:${SERVER_PORT}`,
+                healthCheckUrl: `http://localhost:${SERVER_PORT}/health`,
+                port: {
+                    '$': SERVER_PORT,
+                    '@enabled': true
+                },
+                vipAddress: 'sn-course-service',
+                dataCenterInfo: {
+                    '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+                    'name': 'MyOwn'
+                },
+            },
+            eureka: {
+                host: 'localhost',
+                port: 8761,
+                servicePath: '/eureka/apps'
+            }
+        });
+
+        eurekaClient.start();
+        process.on('SIGINT', () => {
+            // this.app.close();
+            eurekaClient.stop();
+        });
     }
 
     start(callback: Function) {

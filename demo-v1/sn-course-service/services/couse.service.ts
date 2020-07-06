86bf0c4a68d9b4c37febdc3ab83c542a78c0015e
Franco Arratia <franck_ral@hotmail.com>
2020-07-05 23:30:58 -0400

    share-knowledge-course-service: Implementing the required service for Course Microservice

---
+++ demo-v1/sn-course-service/services/couse.service.ts
@@ -0,0 +1,40 @@
+import { CourseModel } from "../models/course.model";
+import { Level } from "../models/level.enum";
+
+export default class CourseService {
+
+    private coursesList: CourseModel[] = [];
+
+    constructor() {
+        this.initializeCoursesInformation();
+    }
+
+
+    getCourses() {
+        return this.coursesList;
+    }
+
+
+    private initializeCoursesInformation() {
+        //
+        const calculoId = '1ffe21b4-bf2e-11ea-b3de-0242ac130004';
+        const programmingId = '1ffe27cc-bf2e-11ea-b3de-0242ac130004';
+        const englishId = '1ffe292a-bf2e-11ea-b3de-0242ac130004';
+        const artificialIntelligenceId = '1ffe2a06-bf2e-11ea-b3de-0242ac130004';
+        const physicsId = '1ffe2b46-bf2e-11ea-b3de-0242ac130004';
+
+        //
+        let calculoCourse = new CourseModel(calculoId, 'Calculo', Level.FIRST_YEAR);
+        let programmingCourse = new CourseModel(programmingId, 'Programming', Level.SECOND_YEAR);
+        let englishCourse = new CourseModel(englishId, 'English', Level.THIRD_YEAR);
+        let artificialIntelligenceCourse = new CourseModel(artificialIntelligenceId, 'Artificial Intelligence', Level.FIFTH_YEAR);
+        let physicsCourse = new CourseModel(physicsId, 'Physics', Level.SECOND_YEAR);
+
+        //
+        this.coursesList.push(calculoCourse);
+        this.coursesList.push(programmingCourse);
+        this.coursesList.push(englishCourse);
+        this.coursesList.push(artificialIntelligenceCourse);
+        this.coursesList.push(physicsCourse);
+    }
+}
